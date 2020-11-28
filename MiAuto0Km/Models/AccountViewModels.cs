f594d27293d4b4b6d2497c02cd0acfc5cc2095d9
jacaceres-97 <jorge97caceres@gmail.com>
2020-11-28 16:21:53 -0300

    About us view has been implemented. Some css changes.

--- MiAuto0Km/Models/AccountViewModels.cs
+++ MiAuto0Km/Models/AccountViewModels.cs
@@ -58,7 +58,7 @@ namespace MiAuto0Km.Models
         [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Recordar mis credenciales")]
         public bool RememberMe { get; set; }
     }
 
@@ -70,14 +70,14 @@ namespace MiAuto0Km.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "La {0} debe tener al menos {2} caracteres.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Contraseña")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
         public string ConfirmPassword { get; set; }
     }
 
