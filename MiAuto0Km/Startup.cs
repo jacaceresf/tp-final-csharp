e88350f1caa87e57c754b4ce247e4646f9bbcf8f
jacaceres-97 <jorge97caceres@gmail.com>
2020-11-28 11:55:18 -0300

    First commitgit statusgit statusgit statusgit statusgit statusgit status

---
+++ MiAuto0Km/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MiAuto0Km.Startup))]
+namespace MiAuto0Km
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
